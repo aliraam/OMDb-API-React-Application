d4b6aaf9d7216199e4fbb5d7628161e93f76e307
aliraam <apirani3784@gmail.com>
2023-04-17 15:42:06 +0430

    add: export custom components

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Header from "./header/header";
+import PosterSlider from "./postSlider/postSlider";
+import ThumbSlider from "./thumbSlider/thumbSlider";
+
+export { Header, PosterSlider, ThumbSlider };
