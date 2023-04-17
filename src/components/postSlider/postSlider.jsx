b69db38760ea5605fbf0c72e9d223291252a320e
aliraam <apirani3784@gmail.com>
2023-04-17 15:28:14 +0430

    add: post slider section

---
+++ src/components/postSlider/postSlider.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Slide } from "./slide";
+import { getTopMovieAPI } from "../../API";
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "./styles.scss";
+
+const PosterSlider = () => {
+  const [movies, setMovies] = useState([]);
+  const getMovies = () => {
+    getTopMovieAPI("a2557d25", "Inception")
+      .then((res) => {
+        setMovies(res.data.Search);
+      })
+      .catch((error) => {
+        setMovies([]);
+      });
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <section className="poster-slider">
+      <Swiper>
+        {movies.map((movie) => (
+          <SwiperSlide key={movie.imdbID}>
+            <Slide posterSrc={movie.Poster} title={movie.Title} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
+};
+
+export default PosterSlider;
